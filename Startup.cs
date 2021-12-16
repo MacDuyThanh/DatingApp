13f027101ebcdbb6d6f04b25e5ab1eedfb40d838
mạc thanh <thanhmac012@gmail.com>
2021-12-16 12:21:54 +0700

    End of section 3

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.OpenApi.Models;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace API
 {
     public class Startup
@@ -39,6 +40,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +56,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
